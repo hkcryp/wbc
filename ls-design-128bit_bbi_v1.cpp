e41962d857f0b061aafdfad8e9c404bab497e4cc
hkcryp <137252389+hkcryp@users.noreply.github.com>
2026-09-13 13:53:51 +0300

    Update ls-design-128bit_bbi_v1.cpp

--- ls-design-128bit_bbi_v1.cpp
+++ ls-design-128bit_bbi_v1.cpp
@@ -289,9 +289,9 @@ void clyde_decrypt(uint32_t* state) {
 
 void generatemessage(uint32_t input[blocknum][CLYDE_NBYTES]){
 	for(int i=0; i<blocknum; i++){
-    for(int j=0; j<CLYDE_NBYTES;j++){
-      input[i][j]=rand();
-    }		
+    	for(int j=0; j<CLYDE_NBYTES;j++){
+      		input[i][j]=rand();
+    	}		
 	}
 }
 
@@ -301,24 +301,25 @@ int main(){
   uint32_t state[blocknum][CLYDE_NBYTES]={};
 
   int iter=100000;
+  unsigned int aux=0;
   uint64_t start_time, end_time, cyc=0;
   srand(time(0));
 
-  generatemessage(input);
   GenerateLBoxTable();
   GenerateXTimeTable();
 
   for(int count=0; count<iter; count ++){
+	generatemessage(input);
     for(int i=0;i<blocknum;i++){
       for(int j=0;j<CLYDE_NBYTES;j++){state[i][j]=input[i][j];}
     }
-    
+
+	start_time=__rdtscp(&aux);
     for(int i=0; i<blocknum;i++){
-      start_time=_rdtsc();
       clyde_decrypt(state[i]);
-      end_time=_rdtsc();
-      cyc+=end_time-start_time;
     }
+	end_time=__rdtscp(&aux);
+    cyc+=end_time-start_time;
   }
 
   uint64_t aver=cyc/iter;
